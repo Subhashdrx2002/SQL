2b19c6a83e709c5a4380b9d8c5c96d85f0a28662
Subhash kumar <123473222+Subhashdrx2002@users.noreply.github.com>
2025-07-06 10:37:22 +0530

    Add files via upload

---
+++ Compare operator.sql
@@ -0,0 +1,15 @@
+SELECT * FROM employees8;
+
+SELECT * FROM employees8
+WHERE age != 30;
+
+
+SELECT * FROM employees8
+WHERE age = 30;
+
+
+SELECT * FROM employees8
+WHERE age >= 30;
+
+SELECT first_name, salary FROM employees8
+WHERE salary > 40000;
---
+++ Logical Operator.sql
@@ -0,0 +1,7 @@
+SELECT * FROM employees8;
+
+SELECT * FROM employees8
+WHERE age>=30 AND salary>=51000;
+
+SELECT * FROM employees8
+WHERE salary>60000  AND age > 28;
